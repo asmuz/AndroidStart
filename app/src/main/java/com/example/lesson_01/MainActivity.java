6585c14f1b74adf2ee17db26f2e9dfac28365d93
AsmuZ <asmuz@mail.ru>
2021-05-27 03:24:14 +0300

    Lesson 1 Task 3

--- app/src/main/java/com/example/lesson_01/MainActivity.java
+++ app/src/main/java/com/example/lesson_01/MainActivity.java
@@ -12,8 +12,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_inputs);
+        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_inputs);
+        //setContentView(R.layout.activity_calendar);
     }
 
 }
