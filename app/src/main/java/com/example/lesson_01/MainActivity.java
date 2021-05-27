91fd2768ff880b353c57c31f9f60fbda0f0152de
AsmuZ <asmuz@mail.ru>
2021-05-27 03:26:40 +0300

    Lesson 1 Task 3

--- app/src/main/java/com/example/lesson_01/MainActivity.java
+++ app/src/main/java/com/example/lesson_01/MainActivity.java
@@ -1,11 +1,8 @@
 package com.example.lesson_01;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
